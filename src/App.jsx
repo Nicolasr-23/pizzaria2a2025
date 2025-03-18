054bcbca1f3230ac3dd4a08f842b0c88c4de9bd0
Nicolas-23 <rm93903@estudante.fieb.edu.br>
2025-03-18 08:27:40 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,25 @@ function App () {
 
   const [nome, setNome] = useState('Ronaldo');
 
+  const Formulário = () => {
+    return (
+      <>
+      <h4>Formulário de Inscrição</h4>
+      <input classname= "Nome"
+      name="nome"
+      placeholder="Digite Seu Nome..."
+      onChange={(e) => setNome(e.target.value)}
+      type="text"/>
+    <button onClick={()=>{alert(nome)}}>
+      CLIQUE AQUI
+    </button>
+      </>
+    )
+  }
   return (
     <div>
       <h3> Pizzaria 2A</h3>
-      <input classname= "Nome"
-        name="nome"
-        placeholder="Digite Seu Nome..."
-        onChange={(e) => setNome(e.target.value)}
-      type="text"/>
-      <button onClick={()=>{alert(nome)}}>
-        CLIQUE AQUI
-      </button>
+      <Formulário/>
     </div>
   )
 }
